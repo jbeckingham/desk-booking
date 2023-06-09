51aa717872e18e97f4f73f714d53deab412242dd
Jennifer Beckingham <jennifer.beckingham@moneyhub.com>
2023-06-09 12:43:38 +0100

    Save credential to cookie

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,30 @@
 import jwtDecode from "jwt-decode"
+
 import "./App.css"
 import "semantic-ui-css/semantic.min.css"
 import {GoogleOAuthProvider} from "@react-oauth/google"
 import App from "./App"
 import {Header, Container} from "semantic-ui-react"
+import moment from "moment"
 
 import {useState, useEffect} from "react"
 import {GoogleLogin} from "@react-oauth/google"
 import logo from "./moneyhub_teal.png"
+import {CookiesProvider} from "react-cookie"
+import {useCookies} from "react-cookie"
 
 const Login = () => {
-  const [credential, setCredential] = useState(null)
+  const [cookies, setCookie] = useCookies(["credential"])
+
+  const [credential, setCredential] = useState(cookies.credential)
+
+  const profile = jwtDecode(credential)
+
+  const onLoginSuccess = credentialResponse => {
+    setCredential(credentialResponse.credential)
+    const expiry = moment().add(1, "days").toDate()
+    setCookie("credential", credentialResponse.credential, {expires: expiry})
+  }
   return (
     <>
       <Container className="logoDiv" textAlign="center">
@@ -24,9 +38,7 @@ const Login = () => {
           <GoogleOAuthProvider clientId="1073576890488-7qnqolkv12d7mkrpsfdu1qo6dv2giogo.apps.googleusercontent.com">
             <GoogleLogin
               onSuccess={credentialResponse => {
-                setCredential(credentialResponse.credential)
-                const profile = jwtDecode(credentialResponse.credential)
-                console.log("User credentials", profile.email, profile.name)
+                onLoginSuccess(credentialResponse)
               }}
               onError={() => {
                 console.log("Login Failed")
